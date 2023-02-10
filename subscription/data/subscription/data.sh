c1c6a0899c9730b149ae25f46eeee7d20e48847d
root <root@ingest.evermight.net>
2023-02-10 16:17:09 +0000

    subscription data

---
+++ subscription/data/subscription/data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+earliestSignupYear=2020
+earliestSignupMonth=4
+signupDurationMonths=13
+maximumActiveMonths=36
+numberOfCustomers=234
+
+echo 'customer_id,pay_date' > data.csv
+for ((c=1; c<=$numberOfCustomers; c++))
+do
+  signupMonth=$(($earliestSignupMonth+($signupDurationMonths-$earliestSignupMonth)*$RANDOM/32767))
+  activeMonths=$(($maximumActiveMonths*$RANDOM/32767))
+  for ((d=0; d<$activeMonths; d++))
+  do
+    currentMonth=$(printf "%02d" $(((($signupMonth+$d)%12)+1)))
+    currentYear=$(($earliestSignupYear+($signupMonth+$d)/12))
+    echo "$c,$currentYear-$currentMonth-01" >> data.csv
+  done
+done

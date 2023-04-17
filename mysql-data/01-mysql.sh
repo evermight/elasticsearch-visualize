8134bb166cd09550cf7afeb1768c740dbd53cefa
root <root@logstash.evermight.net>
2023-04-17 15:51:15 +0000

    consuming mysql data

---
+++ mysql-data/01-mysql.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+source ./.env
+
+hostprotocol="http"
+if [ "$ELASTICSSL" = "true" ]; then
+  hostprotocol="https"
+fi
+
+curl -X PUT -u $ELASTICUSER:$ELASTICPASS "$hostprotocol://$ELASTICHOST/mysql"
+#curl -X PUT -u $ELASTICUSER:$ELASTICPASS "$hostprotocol://$ELASTICHOST/zip_geo/_mapping" \
+#-H "Content-Type: application/json" \
+#-d @$PROJECTPATH/mapping/zip_geo.json
+
+
+
+
+
+logstashconf=`cat ${PROJECTPATH}/logstash/mysql.conf`
+logstashconf="${logstashconf//\#\#JDBCJARFILE\#\#/"$JDBCJARFILE"}"
+logstashconf="${logstashconf//\#\#JDBCCONNSTRING\#\#/"$JDBCCONNSTRING"}"
+logstashconf="${logstashconf//\#\#JDBCUSER\#\#/"$JDBCUSER"}"
+logstashconf="${logstashconf//\#\#JDBCPASS\#\#/"$JDBCPASS"}"
+logstashconf="${logstashconf//\#\#ELASTICHOST\#\#/"$ELASTICHOST"}"
+logstashconf="${logstashconf//\#\#ELASTICSSL\#\#/"$ELASTICSSL"}"
+logstashconf="${logstashconf//\#\#ELASTICUSER\#\#/"$ELASTICUSER"}"
+logstashconf="${logstashconf//\#\#ELASTICPASS\#\#/"$ELASTICPASS"}"
+/usr/share/logstash/bin/logstash -e "$logstashconf"
+
+#curl -X PUT -u $ELASTICUSER:$ELASTICPASS "$hostprotocol://$ELASTICHOST/_enrich/policy/zip_geo_policy" \
+#-H "Content-Type: application/json" \
+#-d @$PROJECTPATH/policy/zip_geo.json
+
+#sleep 30
+#curl -X PUT -u $ELASTICUSER:$ELASTICPASS "$hostprotocol://$ELASTICHOST/_enrich/policy/zip_geo_policy/_execute"
+
+

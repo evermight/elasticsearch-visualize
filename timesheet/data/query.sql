7dceb6af2af4bd9ceed033bfbd07f66122a365a2
root <root@logstash.evermight.net>
2023-04-27 21:08:19 +0000

    ingesting a relational database

--- timesheet/data/query.sql
+++ timesheet/data/query.sql
@@ -1,3 +1,100 @@
+SELECT project.project_id, project.project_name,
+JSON_ARRAYAGG(
+  JSON_OBJECT(
+    'project_id', epic.project_id, 'epic_id', epic.epic_id, 'epic_name', epic.epic_name,
+    'stories', (
+      SELECT JSON_ARRAYAGG(
+        JSON_OBJECT(
+          'epic_id', story.epic_id, 'story_id', story.story_id, 'story_name', story.story_name,
+          'tasks', (
+             SELECT JSON_ARRAYAGG(
+                 JSON_OBJECT('story_id', task.story_id, 'task_id', task.task_id, 'task_name', task.task_name,
+                 'hours', (
+                 SELECT JSON_ARRAYAGG(JSON_OBJECT('task_id', time.task_id, 'user_id', time.user_id, 'hours', time.hours))
+                 FROM time
+                 WHERE time.task_id = task.task_id
+                )
+              )
+            )
+            FROM task
+            WHERE task.story_id = story.story_id
+          )
+        )
+      ) FROM story
+    WHERE story.epic_id = epic.epic_id
+    )
+  )
+) AS epics
+FROM project
+INNER JOIN epic ON epic.project_id = project.project_id
+GROUP BY project.project_id;
+
+SELECT epic.epic_id, epic.epic_name,
+JSON_ARRAYAGG(
+  JSON_OBJECT(
+    'epic_id', story.epic_id, 'story_id', story.story_id, 'story_name', story.story_name,
+    'tasks', (
+      SELECT JSON_ARRAYAGG(
+	JSON_OBJECT('story_id', task.story_id, 'task_id', task.task_id, 'task_name', task.task_name,
+          'hours', (
+            SELECT JSON_ARRAYAGG(JSON_OBJECT('task_id', time.task_id, 'user_id', time.user_id, 'hours', time.hours))
+            FROM time
+            WHERE time.task_id = task.task_id
+	  )
+	)
+      )
+      FROM task
+      WHERE task.story_id = story.story_id
+    )
+  )
+) AS epics
+FROM project
+INNER JOIN epic ON epic.project_id = project.project_id
+GROUP BY project.project_id;
+
+SELECT epic.epic_id, epic.epic_name,
+JSON_ARRAYAGG(
+  JSON_OBJECT(
+    'epic_id', story.epic_id, 'story_id', story.story_id, 'story_name', story.story_name,
+    'tasks', (
+      SELECT JSON_ARRAYAGG(
+	JSON_OBJECT('story_id', task.story_id, 'task_id', task.task_id, 'task_name', task.task_name,
+          'hours', (
+            SELECT JSON_ARRAYAGG(JSON_OBJECT('task_id', time.task_id, 'user_id', time.user_id, 'hours', time.hours))
+            FROM time
+            WHERE time.task_id = task.task_id
+	  )
+	)
+      )
+      FROM task
+      WHERE task.story_id = story.story_id
+    )
+  )
+) AS stories
+FROM epic
+INNER JOIN story ON story.epic_id = epic.epic_id
+GROUP BY epic.epic_id
+LIMIT 3;
+
+SELECT story.story_id, story_name,
+JSON_ARRAYAGG(
+  JSON_OBJECT(
+    'story_id', task.story_id, 'task_id', task.task_id, 'task_name', task.task_name,
+    'hours', (
+      SELECT JSON_ARRAYAGG(JSON_OBJECT('task_id', time.task_id, 'user_id', time.user_id, 'hours', time.hours))
+      FROM time
+      WHERE time.task_id = task.task_id
+    )
+  )
+) AS tasks
+FROM story
+INNER JOIN task ON task.story_id = story.story_id
+GROUP BY story.story_id
+LIMIT 3;
+
+
+
+
 SELECT story.story_id, story_name,
 JSON_ARRAYAGG(
   JSON_OBJECT(

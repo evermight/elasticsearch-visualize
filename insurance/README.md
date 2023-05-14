234e7d645de9397d1c890eccb5fee6d115269052
root <root@reporting.evermight.net>
2023-05-14 11:32:41 +0000

    rain data

---
+++ insurance/README.md
@@ -0,0 +1,10 @@
+Take Rainer's CSV file and make sure purchase date column is M/d/yyyy and all money columns do not have $ symbol.
+
+WEB
+
+- Then upload through machine learning
+- Use the mapping/rainerdata.json
+
+CLI
+
+Then use the bash scripts from command line to import views and dashboards
--- insurance/dashboard-import.sh
+++ insurance/dashboard-import.sh
@@ -7,4 +7,4 @@ if [ "$ELASTICSSL" = "true" ]; then
   hostprotocol="https"
 fi
 
-curl -X POST -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/saved_objects/_import?createNewCopies=true" -H "kbn-xsrf: true" --form file=@./kibana/dashboard/management.ndjson
+curl -X POST -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/saved_objects/_import?createNewCopies=true" -H "kbn-xsrf: true" --form file=@./kibana/dashboard/rainerdata.ndjson

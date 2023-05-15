a5360fd22079f5cecbf6b2035cad7ebd7e44e8e7
root <root@reporting.evermight.net>
2023-05-15 16:43:19 +0000

    story imports

--- management/03-snowflake/reportdown.sh
+++ management/03-snowflake/reportdown.sh
@@ -17,3 +17,4 @@ curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/_i
 
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/23232963-fff0-4037-badf-3e355cf42ec0" -H "kbn-xsrf: reporting"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/26b1c9b2-d3b2-4db3-84af-cf10ba93f8c6" -H "kbn-xsrf: reporting"
+curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/c00b8f8b-f5a6-487a-986a-7166a8cbb122" -H "kbn-xsrf: reporting"

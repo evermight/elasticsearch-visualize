d064c72c09f7fd8eeffe86c958152b72e0500d37
root <root@reporting.evermight.net>
2023-05-15 18:52:52 +0000

    temporary epics

---
+++ management/03-snowflake/report-all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./report.sh -d timesheet;
+./report.sh -d work;
+./report.sh -d report-story;
+./report.sh -d report-epic;
--- management/03-snowflake/reportdown.sh
+++ management/03-snowflake/reportdown.sh
@@ -10,11 +10,14 @@ fi
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/${INDEXNAME}_work"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/${INDEXNAME}_timesheet"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/${INDEXNAME}_report-story"
+curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/${INDEXNAME}_report-epic"
 
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/_ingest/pipeline/${INDEXNAME}_work"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/_ingest/pipeline/${INDEXNAME}_timesheet"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/_ingest/pipeline/${INDEXNAME}_report-story"
+curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${ELASTICHOST}/_ingest/pipeline/${INDEXNAME}_report-epic"
 
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/23232963-fff0-4037-badf-3e355cf42ec0" -H "kbn-xsrf: reporting"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/26b1c9b2-d3b2-4db3-84af-cf10ba93f8c6" -H "kbn-xsrf: reporting"
 curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/c00b8f8b-f5a6-487a-986a-7166a8cbb122" -H "kbn-xsrf: reporting"
+curl -X DELETE -u $ELASTICUSER:$ELASTICPASS "${hostprotocol}://${KIBANAHOST}/api/data_views/data_view/b50d3948-0900-4617-b8fe-89dac07340fd" -H "kbn-xsrf: reporting"
